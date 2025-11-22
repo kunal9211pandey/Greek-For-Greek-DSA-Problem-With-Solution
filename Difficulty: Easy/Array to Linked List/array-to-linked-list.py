e0af5c0643eba998571c8509adfa27d49ec5845f
Kunal Pandey <118272078+kunal9211pandey@users.noreply.github.com>
2025-11-22 18:17:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array to Linked List/array-to-linked-list.py
+++ Difficulty: Easy/Array to Linked List/array-to-linked-list.py
@@ -1,57 +1,29 @@
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-# } Driver Code Ends
-
-#User function Template for python3
-
+'''
 # Node Class
 class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-        
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+
 class Solution:
-    def constructLL(self, arr):
+    def arrayToList(self, arr):
         # code here
         if not arr:
             return None
-        
-        head=Node(arr[0])
-        current=head
-        for i in arr[1:]:
-            current.next=Node(i)
-            current = current.next
-        
-        return head
-    
-    def printLL(head):
-        current=head
-        
-        while current:
-            print(data , end='->'if current.next else "\n")
-            current=current.next
             
-
-
-#{ 
- # Driver Code Starts.
-class Node:
-    def __init__(self, data):   # data -> value stored in node
-        self.data = data
-        self.next = None
-
-if __name__ == '__main__': 
-    t = int(input ())
-    for _ in range (t):
-        # n = int(input())
-        arr = list(map(int, input().split()))
-        ob = Solution()
-        res = ob.constructLL(arr)
-        while res:
-            print(res.data, end = ' ')
-            res = res.next
-        print()
-        print("~")
-# } Driver Code Ends
+        head = Node(arr[0])
+        current = head
+        
+        for val in arr[1:]:
+            current.next = Node(val)
+            current = current.next
+        return head
+        
+    def print_ll(head):
+        current = head
+        while current:
+            print(current.data , end ="->" if current.next else "\n")
+        
+        
+        
