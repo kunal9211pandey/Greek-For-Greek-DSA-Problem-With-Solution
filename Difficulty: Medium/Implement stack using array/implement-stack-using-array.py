a35335358cbe246288e62a36b8f876918e02f837
Kunal Pandey <118272078+kunal9211pandey@users.noreply.github.com>
2025-12-03 05:18:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement stack using array/implement-stack-using-array.py
@@ -0,0 +1,29 @@
+class myStack:
+    def __init__(self, n):
+        self.arr = [0] * n
+        self.capacity = n
+        self.top = -1
+
+    def isEmpty(self):
+        return self.top == -1
+
+    def isFull(self):
+        return self.top == self.capacity - 1
+
+    def push(self, x):
+        if self.isFull():
+            return
+        self.top += 1
+        self.arr[self.top] = x
+
+    def pop(self):
+        if self.isEmpty():
+            return -1
+        val = self.arr[self.top]
+        self.top -= 1
+        return val
+
+    def peek(self):
+        if self.isEmpty():
+            return -1
+        return self.arr[self.top]
