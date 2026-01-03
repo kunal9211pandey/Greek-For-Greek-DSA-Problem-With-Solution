ed107c4cd74951c6a29da8a2f762cf773389e50f
Kunal Pandey <118272078+kunal9211pandey@users.noreply.github.com>
2026-01-03 22:40:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.py
+++ Difficulty: Basic/Reverse a String/reverse-a-string.py
@@ -3,26 +3,12 @@
 class Solution:
      def reverseString(self, s: str) -> str:
         # code here
-        n = len(s)
-        reversed_str = ""
-        for i in range(n - 1, -1, -1):  # Iterate from last index to first
-            reversed_str += s[i]
-        return reversed_str
-            
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == "__main__":
-    t = int(input())
-    while (t > 0):
-        s = input()
-        ob = Solution()
-        print(ob.reverseString(s))
-        t = t - 1
-
-        print("~")
-
-# } Driver Code Ends
+        char_list = list(s)
+        i = 0
+        j = len(s)-1
+        
+        while i < j:
+            char_list[i], char_list[j] = char_list[j], char_list[i]
+            i += 1
+            j -= 1
+        return "".join(char_list)
