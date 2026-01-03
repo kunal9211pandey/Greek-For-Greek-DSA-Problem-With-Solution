9e3e31bc76464743b231e3d84d8c96ec0070efb6
Kunal Pandey <118272078+kunal9211pandey@users.noreply.github.com>
2026-01-03 18:57:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Elements of Array/print-elements-of-array.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+class Solution:
+    # Just print the space seperated array elements
+	def printArray(self, arr):
+	    # code here
+	    for i in range(len(arr)):
+	        print(arr[i] ,end=' ')
