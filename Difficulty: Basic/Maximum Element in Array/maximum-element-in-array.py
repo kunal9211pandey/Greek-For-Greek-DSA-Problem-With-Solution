ad856ee1e93d9dc2e7e38fd783122a4c26f8e44a
Kunal Pandey <118272078+kunal9211pandey@users.noreply.github.com>
2026-01-04 22:05:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Maximum Element in Array/maximum-element-in-array.py
@@ -0,0 +1,14 @@
+
+from typing import List
+
+
+class Solution:
+    def largest(self, arr : List[int]) -> int:
+        # code here
+        max_element = arr[0]
+        
+        for i in range(1, len(arr)):
+            if arr[i] > max_element:
+                max_element = arr[i]
+        return max_element
+        
