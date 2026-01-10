16b4abe8ee066a820695e119d79bc2c9dc1595e5
Kunal Pandey <118272078+kunal9211pandey@users.noreply.github.com>
2026-01-10 22:30:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove character/remove-character.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+class Solution:
+    def removeChars (ob, str1, str2):
+        # code here 
+        result = []
+        for ch in str1:
+            if ch not in str2:
+                result.append(ch)
+        return "".join(result)
