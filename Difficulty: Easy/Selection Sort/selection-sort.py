96665af9a142757584964c295296826530c38ce9
Kunal Pandey <118272078+kunal9211pandey@users.noreply.github.com>
2026-01-23 09:19:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Selection Sort/selection-sort.py
+++ Difficulty: Easy/Selection Sort/selection-sort.py
@@ -1,47 +1,14 @@
-#User function Template for python3
-
-class Solution: 
+class Solution:
     def selectionSort(self, arr):
-        n=len(arr)
+        n = len(arr)
+        
         for i in range(n):
-            min_index=i
-            for j in range(i+1 , n):
-                if arr[j] < arr[min_index]:
-                    min_index=j
-            arr[i],arr[min_index]=arr[min_index],arr[i]
-        return arr
-                    
+            min_i = i
             
+            for j in range(i + 1, n):
+                if arr[j] < arr[min_i]:
+                    min_i = j
             
-
-
-#{ 
- # Driver Code Starts
-class IntArray:
-
-    def __init__(self) -> None:
-        pass
-
-    def Input(self, n):
-        arr = [int(i) for i in input().strip().split()]  #array input
+            arr[i], arr[min_i] = arr[min_i], arr[i]
+        
         return arr
-
-    def Print(self, arr):
-        for i in arr:
-            print(i, end=" ")
-        print()
-
-
-if __name__ == "__main__":
-    t = int(input())
-    for _ in range(t):
-
-        arr = [int(i) for i in input().split()]
-
-        obj = Solution()
-        obj.selectionSort(arr)
-
-        IntArray().Print(arr)
-        print("~")
-
-# } Driver Code Ends
