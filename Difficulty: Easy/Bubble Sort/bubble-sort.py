f8a87eea70eb841b4ae21d0386e56777a1562e81
Kunal Pandey <118272078+kunal9211pandey@users.noreply.github.com>
2026-01-23 09:22:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bubble Sort/bubble-sort.py
+++ Difficulty: Easy/Bubble Sort/bubble-sort.py
@@ -1,31 +1,9 @@
-#User function Template for python3
-
 class Solution:
-    #Function to sort the array using bubble sort algorithm.
     def bubbleSort(self,arr):
-        n=len(arr)
-        for i in range(0 , n):
-            for j in range(i+1 , n):
-                if arr[i]>arr[j]:
-                    arr[i],arr[j]=arr[j],arr[i]
-        return arr
-            
         # code here
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    t = int(input())
-    for i in range(t):
-
-        arr = list(map(int, input().strip().split()))
-        ob = Solution()
-        ob.bubbleSort(arr)
-        for i in arr:
-            print(i, end=' ')
-        print()
-
-# } Driver Code Ends
+        n = len(arr)
+        for i in range(n):
+            for j in range(n-i-1):
+                if arr[j] > arr[j + 1]:
+                    arr[j] , arr[j + 1] = arr[j + 1] , arr[j]
+        return arr
