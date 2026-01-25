00f7000e2ad3868c3bd1b8fad50b01a6b924fb25
Kunal Pandey <118272078+kunal9211pandey@users.noreply.github.com>
2026-01-25 19:32:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Search in Linked List/search-in-linked-list.py
+++ Difficulty: Easy/Search in Linked List/search-in-linked-list.py
@@ -1,16 +1,11 @@
-'''
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-'''
-
 class Solution:
     def searchKey(self, head, key):
-        #Code here
-        current = head
-        while current is not None:
-            if current.data == key:
+        if head is None:
+            return False
+
+        while head is not None:
+            if head.data == key:
                 return True
-            current = current.next
+            head = head.next
+
         return False
